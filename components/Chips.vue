27ca40dda5bb02f6b1ea4fe89267235408cbe4a4
Naris <naris@odity.fr>
2020-11-17 15:37:50 +0300

    ajout script medecin

--- components/Chips.vue
+++ components/Chips.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="inline-block rounded-3xl bg-gray-400 text-gray-700 py-2 px-4 mt-1">
-    <span>Dentisterie</span>
-    <span class="hover:text-red-600 cursor-pointer">
+  <div class="inline-block rounded-3xl bg-gray-400 text-gray-700 py-2 px-4 mt-1" v-if="isShown">
+    <span>{{ content }}</span>
+    <span class="hover:text-red-600 cursor-pointer" @click="closeChip">
       <svg class="w-6 h-6 ml-1 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
     </span>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    content: String,
+  },
+  data() {
+    return {
+      isShown: true,
+    }
+  },
+  methods: {
+    closeChip() {
+      this.isShown = false
+    }
+  }
+}
+</script>
--- pages/inscription/medecin.vue
+++ pages/inscription/medecin.vue
@@ -10,7 +10,7 @@
           </label>
         </div>
         <div>
-          <select class="form-select">
+          <select class="form-select" v-model="title">
             <option value="h">Professeur</option>
             <option value="m">Docteur</option>
           </select>
@@ -23,7 +23,7 @@
           </label>
         </div>
         <div class="md:w-3/4">
-          <input class="form-input w-full" type="text" value="Jane Doe">
+          <input class="form-input w-full" type="text" value="Doe" v-model="name">
         </div>
       </div>
       <div class="md:flex md:items-center mb-6">
@@ -33,7 +33,7 @@
           </label>
         </div>
         <div class="md:w-3/4">
-          <input class="form-input w-full" type="text" value="Jane Doe">
+          <input class="form-input w-full" type="text" value="Jane" v-model="first_name">
         </div>
       </div>
       <div class="md:flex md:items-center mb-6">
@@ -43,7 +43,7 @@
           </label>
         </div>
         <div class="md:w-3/4 flex">
-          <input class="form-input w-1/2" type="text" value="Jane Doe">
+          <input class="form-input w-1/2" type="text" value="Jane Doe" v-model="profil">
           <button class="btn-green flex">
             <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path></svg>
             Choisir un fichier
@@ -57,7 +57,7 @@
           </label>
         </div>
         <div class="md:w-3/4">
-          <input class="form-input w-full" type="number" value="Jane Doe">
+          <input class="form-input w-full" type="number" value="Jane Doe" v-model="onm">
         </div>
       </div>
       <div class="md:flex mb-6">
@@ -68,14 +68,14 @@
         </div>
         <div class="md:w-3/4">
           <div class="flex">
-            <input class="form-input w-1/2" type="text" value="Jane Doe">
-            <button class="btn-green flex">
+            <input class="form-input w-1/2" type="text" value="Jane Doe" v-model="speciality">
+            <button class="btn-green flex" @click="addSpeciality">
               <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
               Ajouter
             </button>
           </div>
           <div class="w-auto">
-            <Chips />
+            <Chips v-for="speciality in specialities" :key="speciality" :content="speciality" class="mr-1" />
           </div>
         </div>
       </div>
@@ -88,13 +88,13 @@
           </div>
           <div class="md:w-3/4">
             <div class="md:w-3/4 mb-1">
-              <input class="form-input w-full" type="text" placeholder="Région">
+              <input class="form-input w-full" type="text" placeholder="Région" v-model="region">
             </div>
             <div class="md:w-3/4 mb-1">
-              <input class="form-input w-full" type="text" placeholder="Commune">
+              <input class="form-input w-full" type="text" placeholder="Commune" v-model="commune">
             </div>
             <div class="md:w-3/4 mb-1">
-              <input class="form-input w-full" type="text" placeholder="Logement">
+              <input class="form-input w-full" type="text" placeholder="Logement" v-model="logement">
             </div>
             <div class="md:w-3/4 flex items-center">
               <input class="form-input w-full" type="text" placeholder="Coordonnées géographiques">
@@ -110,7 +110,16 @@
           </label>
         </div>
         <div class="md:w-3/4">
-          <input class="form-input w-full" type="text" value="Jane Doe">
+          <div class="flex">
+            <input class="form-input w-1/2" type="text" value="Jane Doe" v-model="mail">
+            <button class="btn-green flex" @click="addMail">
+              <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              Ajouter
+            </button>
+          </div>
+          <div class="w-auto">
+            <Chips v-for="mail in mails" :key="mail" v-bind:content="mail" class="mr-1"/>
+          </div>
         </div>
       </div>
       <div class="md:flex mb-6">
@@ -121,20 +130,20 @@
         </div>
         <div class="md:w-3/4">
           <div class="flex">
-            <input class="form-input w-1/2" type="number" value="Jane Doe">
-            <button class="btn-green flex">
+            <input class="form-input w-1/2" type="text" value="Jane Doe" v-model="phone">
+            <button class="btn-green flex" @click="addPhone">
               <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
               Ajouter
             </button>
           </div>
           <div class="w-auto">
-            <Chips />
+            <Chips v-for="phone in phones" :key="phone" v-bind:content="phone" class="mr-1"/>
           </div>
         </div>
       </div>
       <div class="md:flex mb-2">
         <div class="md:w-1/6 mt-2">
-          <label class="form-label" for="inline-full-name">
+          <label class="form-label" for="inline-full-name" v-html="presentation">
             Présentation
           </label>
         </div>
@@ -186,7 +195,7 @@
           </label>
         </div>
         <div class="md:w-3/4">
-          <input class="form-input w-full" type="text" value="Jane Doe">
+          <input class="form-input w-full" type="text" value="Jane Doe" v-model="facebook">
         </div>
       </div>
       <div class="md:flex md:items-center mb-6">
@@ -196,7 +205,7 @@
           </label>
         </div>
         <div class="md:w-3/4">
-          <input class="form-input w-full" type="text" value="Jane Doe">
+          <input class="form-input w-full" type="text" value="Jane Doe" v-model="twitter">
         </div>
       </div>
       <div class="md:flex md:items-center mb-2">
@@ -206,7 +215,7 @@
           </label>
         </div>
         <div class="md:w-3/4">
-          <input class="form-input w-full" type="text" value="Jane Doe">
+          <input class="form-input w-full" type="text" value="Jane Doe" v-model="linkedin">
         </div>
       </div>
     </div>
@@ -232,7 +241,25 @@ export default {
   },
   data() {
     return {
-      showModal: false
+      showModal: false,
+      title: '',
+      name: '',
+      first_name: '',
+      profil: '', 
+      onm: '',
+      speciality: '',
+      specialities: [],
+      region: '',
+      commune: '',
+      logement: '',
+      mail: '',
+      mails: [],
+      phone: '',
+      phones: [],
+      presentation: '',
+      facebook: '',
+      twitter: '',
+      linkedin: ''
     }
   },
   methods: {
@@ -240,7 +267,60 @@ export default {
       this.$route.push({
         path: path
       })
+    },
+    addPhone() {
+      if (this.phone)
+        this.phones.push(this.phone)
+    },
+    addMail() {
+      if (this.mail)
+        this.mails.push(this.mail)
+    },
+    addSpeciality() {
+      if (this.speciality)
+        this.specialities.push(this.speciality)
+    },
+    storeData() {
+      if (process.browser) {
+        localStorage.setItem('title', this.title)
+        localStorage.setItem('name', this.name)
+        localStorage.setItem('first_name', this.first_name)
+        localStorage.setItem('profil', this.profil)
+        localStorage.setItem('onm', this.onm)
+        localStorage.setItem('region', this.region)
+        localStorage.setItem('commune', this.commune)
+        localStorage.setItem('logement', this.logement)
+        localStorage.setItem('presentation', this.presentation)
+        localStorage.setItem('facebook', this.facebook)
+        localStorage.setItem('twitter', this.twitter)
+        localStorage.setItem('linkedin', this.linkedin)
+        localStorage.setItem('specialities', JSON.stringify(this.specialities))
+        localStorage.setItem('mails', JSON.stringify(this.mails))
+        localStorage.setItem('phones', JSON.stringify(this.phones))
+      }
+    },
+    fillForm() {
+      if (process.browser) {
+        this.title = localStorage.getItem('title') ? localStorage.getItem('title') : ''
+        this.name = localStorage.getItem('name') ? localStorage.getItem('name') : ''
+        this.first_name = localStorage.getItem('first_name') ? localStorage.getItem('first_name') : ''
+        this.profil = localStorage.getItem('profil') ? localStorage.getItem('profil') : ''
+        this.onm = localStorage.getItem('onm') ? localStorage.getItem('onm') : ''
+        this.region = localStorage.getItem('region') ? localStorage.getItem('region') : ''
+        this.commune = localStorage.getItem('commune') ? localStorage.getItem('commune') : ''
+        this.logement = localStorage.getItem('logement') ? localStorage.getItem('logement') : ''
+        this.presentation = localStorage.getItem('presentation') ? localStorage.getItem('presentation') : ''
+        this.facebook = localStorage.getItem('facebook') ? localStorage.getItem('facebook') : ''
+        this.twitter = localStorage.getItem('twitter') ? localStorage.getItem('twitter') : ''
+        this.linkedin = localStorage.getItem('linkedin') ? localStorage.getItem('linkedin') : ''
+        this.specialities = localStorage.getItem('specialities') ? JSON.parse(localStorage.getItem('specialities')) : []
+        this.mails = localStorage.getItem('mails') ? JSON.parse(localStorage.getItem('mails')) : []
+        this.phones = localStorage.getItem('phones') ? JSON.parse(localStorage.getItem('phones')) : []
+      }
     }
+  },
+  mounted() {
+    this.fillForm()
   }
 }
 </script>
